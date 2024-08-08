01999e1dfe5416261327c9399f662a79e294555e
Thanaphat Sanngoen <blazesonic14@gmail.com>
2024-08-08 20:24:16 +0700

    6.3

---
+++ 331-intro-to-vite/src/types.ts
@@ -0,0 +1,28 @@
+export interface Event {
+  id: number
+  category: string
+  title: string
+  location: string
+  date: string
+  time: string
+  petsAllowed: boolean
+  organizer: string
+  studentId: string
+  gpa: number
+  name: string
+  surname: string
+}
+export interface Student {
+  id: number
+  category: string
+  title: string
+  location: string
+  date: string
+  time: string
+  petsAllowed: boolean
+  organizer: string
+  studentId: string
+  gpa: number
+  name: string
+  surname: string
+}
